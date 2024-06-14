5802e29a9001b20ec2625170564eb45788c518fb
vnlvi2k3 <139733764+vnlvi2k3@users.noreply.github.com>
2024-06-14 12:35:47 +0700

    Update GetFeed.ts

--- action/GetFeed.ts
+++ action/GetFeed.ts
@@ -1,5 +1,5 @@
 export default async function GetFeed(feed: string, formData?: FormData) {
-    const response = await fetch(`https://io.adafruit.com/api/v2/${process.env.NEXT_PUBLIC_ADA_USERNAME}/feeds/${feed}/data`, {
+    const response = await fetch(`https://io.adafruit.com/api/v2/${process.env.NEXT_PUBLIC_ADA_USERNAME}/feeds/${feed}/data?limit=5`, {
         method: "GET",
         mode: "cors",
         cache: "no-cache",
